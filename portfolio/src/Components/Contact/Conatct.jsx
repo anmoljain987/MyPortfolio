be425701f429a54601191cb0f9bcf0df06cf9fe4
Anmol <anmoljain987@gmail.com>
2022-04-09 17:39:48 +0530

    some changes

--- portfolio/src/Components/Contact/Conatct.jsx
+++ portfolio/src/Components/Contact/Conatct.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import { Container } from "react-bootstrap";
 import Particle from "../Particle";
 function Conatct() {
-  return <Container fluid className="contact-section">
-    <Particle/>
-  </Container>;
+  return (
+    <Container fluid className="contact-section">
+      <Particle />
+    </Container>
+  );
 }
 
 export default Conatct;
